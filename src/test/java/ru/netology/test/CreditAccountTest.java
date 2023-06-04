108a6156fbb1d4c6d206bbcbd7439b674f1734ea
Yackov <yackov.irkyt@gmail.com>
2023-06-04 21:06:47 +0500

    Added test for add method

--- src/test/java/ru/netology/test/CreditAccountTest.java
+++ src/test/java/ru/netology/test/CreditAccountTest.java
@@ -18,4 +18,95 @@ public class CreditAccountTest {
 
         Assertions.assertEquals(3_000, account.getBalance());
     }
+
+    @Test
+    public void shouldAddToZeroBalance() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        account.add(0);
+
+        Assertions.assertEquals(0, account.getBalance());
+    }
+
+    @Test
+    public void shouldAddToNegativeBalance() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        account.add(-3_000);
+
+        Assertions.assertEquals(0, account.getBalance());
+    }
+
+    @Test
+    public void shouldAddToMoreCreditLimit() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        account.add(6_000);
+
+        Assertions.assertEquals(0, account.getBalance());
+    }
+
+    @Test
+    public void shouldAddToMoreCreditLimitWhenBalanceGreaterZero() {
+        CreditAccount account = new CreditAccount(
+                1000,
+                5_000,
+                15
+        );
+
+        account.add(6_000);
+
+        Assertions.assertEquals(1000, account.getBalance());
+    }
+
+    @Test
+    public void shouldAddToPositiveMoneyWhenBalanceGreaterZero() {
+        CreditAccount account = new CreditAccount(
+                1000,
+                5_000,
+                15
+        );
+
+        account.add(3_000);
+
+        Assertions.assertEquals(4000, account.getBalance());
+    }
+
+    @Test
+    public void shouldAddToNegativeMoneyWhenBalanceGreaterZero() {
+        CreditAccount account = new CreditAccount(
+                1000,
+                5_000,
+                15
+        );
+
+        account.add(-3_000);
+
+        Assertions.assertEquals(1000, account.getBalance());
+    }
+
+    @Test
+    public void shouldAddToZeroMoneyWhenBalanceGreaterZero() {
+        CreditAccount account = new CreditAccount(
+                1000,
+                5_000,
+                15
+        );
+
+        account.add(0);
+
+        Assertions.assertEquals(1000, account.getBalance());
+    }
 }
