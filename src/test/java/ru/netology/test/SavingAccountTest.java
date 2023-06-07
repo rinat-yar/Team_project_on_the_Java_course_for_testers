fe720e9e8022c1183b3d0eedf1e310079040aa79
Rinat <rinat-yar@mail.ru>
2023-06-07 11:53:47 +0300

    Initial commit_1

--- src/test/java/ru/netology/test/SavingAccountTest.java
+++ src/test/java/ru/netology/test/SavingAccountTest.java
@@ -19,4 +19,165 @@ public class SavingAccountTest {
 
         Assertions.assertEquals(2_000 + 3_000, account.getBalance());
     }
+
+    @Test
+    public void testMaxBalance_1() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+        account.add(8_000);
+
+        Assertions.assertEquals(10_000, account.getBalance());
+    }
+
+    @Test // ++граничные значения, на 1 больше макс
+    public void testMaxBalance_2() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+        account.add(8_001);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+
+    @Test // ++граничные значения, на 1 меньше макс
+    public void testMaxBalance_3() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+        account.add(7_999);
+
+        Assertions.assertEquals(9_999, account.getBalance());
+    }
+
+    @Test //++ прибавляем произвольное число, которое превышает лимит
+    public void testMaxBalance_4() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+        account.add(10_000);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+
+    @Test // ++негативное тестироване на прибавление отрицательного числа
+    public void testNegative() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+        account.add(-1000);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+
+    @Test // ++ списание произвольной отрицательной суммы
+    public void testNegative_1() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.pay(-10_000);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+
+    @Test // ++ граничные значения по списанию
+    public void testBoundaryValues() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.pay(1_000);
+
+        Assertions.assertEquals(1_000, account.getBalance());
+    }
+
+    @Test //++ граничные значения, на 1 меньше минимума
+    public void testBoundaryValues_1() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.pay(1_001);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+
+    @Test //++ граничные значения, на 1 больше минимума
+    public void testBoundaryValues_2() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.pay(999);
+
+        Assertions.assertEquals(1_001, account.getBalance());
+    }
+
+    @Test
+    public void testMinBalance() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+
+        Assertions.assertEquals(1000, account.getMinBalance());
+    }
+
+    @Test
+    public void testMaxBalance() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+
+        Assertions.assertEquals(10_000, account.getMaxBalance());
+    }
+
+    @Test
+    public void testYearChange() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.yearChange();
+
+        Assertions.assertEquals(2_100, account.yearChange());
+    }
 }
